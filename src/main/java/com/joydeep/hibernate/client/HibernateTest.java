7b9b897b6d08e1b2d97d40be4f5522b5192fe16e
Joydeep CHowdhury <joydeep.chowdhury@wipro.com>
2020-01-06 22:10:09 +0530

    Initial commit

---
+++ src/main/java/com/joydeep/hibernate/client/HibernateTest.java
@@ -0,0 +1,27 @@
+package com.joydeep.hibernate.client;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.joydeep.hibernate.model.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+        SessionFactory sf=new Configuration().configure().buildSessionFactory();
+        Session session=sf.openSession();
+        session.beginTransaction();
+        Query query=session.createQuery("from UserDetails where userId>5");
+        List<UserDetails> users=query.list();
+        
+        session.getTransaction().commit();
+        session.close();
+        System.out.println(users);
+        //Temp is now detached and not racked by hibernate since session is closed
+	}
+
+}
